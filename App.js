fd1b86323c7641834a147cbc496051837ba5cbb6
tharinduAyagama <tharinduhirantha32@gmail.com>
2020-07-18 14:43:48 +0530

    scrollview and list

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native';
 
 export default function App() {
 
   const [name , setName] = useState('tharindu');
   const [age , setAge] = useState(10);
   const [person , setPerson] = useState({name: 'tharindu' , age: 20});
+  const [people , setPeople] = useState([
+    {key:1 , name:'tharindu'},
+    {key:2 , name:'hirantha'},
+    {key:3 , name:'bandara'},
+    {key:4 , name:'ayagama'},
+    {key:5 , name:'saman'},
+    {key:6 , name:'uku'},
+    {key:7 , name:'raba'}
+  ]);
 
   const clickHandler = () => {
     setName('hirantha');
@@ -21,19 +30,31 @@ export default function App() {
     //     </View>
     // </View>
 
-    <View style={styles.container}>
-      <Text>Name:</Text>
-      <TextInput
-        style = {styles.input}
-        placeholder = 'mark'
-        onChangeText = {(val) => setName(val)} />
-      <Text>Age:</Text>
-      <TextInput
-        keyboardType = 'numeric' 
-        style = {styles.input}
-        placeholder = '23'
-        onChangeText = {(val) => setAge(val)} />
-      <Text>ur name is {name} and age is {age}</Text>
+    // <View style={styles.container}>
+    //   <Text>Name:</Text>
+    //   <TextInput
+    //     style = {styles.input}
+    //     placeholder = 'mark'
+    //     onChangeText = {(val) => setName(val)} />
+    //   <Text>Age:</Text>
+    //   <TextInput
+    //     keyboardType = 'numeric' 
+    //     style = {styles.input}
+    //     placeholder = '23'
+    //     onChangeText = {(val) => setAge(val)} />
+    //   <Text>ur name is {name} and age is {age}</Text>
+    // </View>
+
+    <View>
+      <ScrollView>
+        {people.map(
+          (item) => (
+            <View key = {item.key} >
+              <Text style={styles.item}>{item.name}</Text>
+            </View>
+        )
+        )}
+      </ScrollView>
     </View>
   );
 }
@@ -53,5 +74,11 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     borderWidth: 1,
     width: 200
+  },
+  item: {
+    marginTop: 30,
+    padding: 30,
+    fontSize: 30,
+    backgroundColor: 'orange'
   }
 });
