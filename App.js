c0473364bf32973bbf76729e33529793dae5f94f
tharinduAyagama <tharinduhirantha32@gmail.com>
2020-07-18 13:53:53 +0530

    text view and styles

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+
+  const [name , setName] = useState('tharindu');
+  const [age , setAge] = useState(10);
+  const [person , setPerson] = useState({name: 'tharindu' , age: 20});
+
+  const clickHandler = () => {
+    setName('hirantha');
+    setPerson({name: 'hirantha' , age: 40});
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <Text>my name is {name}</Text>
+        <Text>my name is {person.name} and my age is {person.age}</Text>
+        <View style={styles.button}>
+          <Button title='change states' onPress={clickHandler} />
+        </View>
     </View>
   );
 }
@@ -18,4 +30,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    marginTop: 30
+  },
+  input: {
+    padding: 5,
+    borderColor: 'black',
+    borderWidth: 1,
+    width: 200
+  }
 });
